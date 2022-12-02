0809afbba7f56f5e5400e52d4091eb3c08bbbe32
sachin-11 <rajeshsachin786@gmail.com>
2022-12-03 00:21:23 +0530

    updated commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello this is first response using node js')
+    res.send('hi this is updated')
 })
 
 
